111538fb649abf9d71fee6036d2f86a05ce0530c
code2913 <smagafu@gmail.com>
2021-10-08 13:30:51 +0300

    updated services

--- pages/urls.py
+++ pages/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('insuarance', views.Insuarance.as_view(),name="insuarance"),
     path('service/air-condition', views.ACService.as_view(),name="ac"),
     path('service/brake-suspension', views.BrakeSuspension.as_view(),name="brakes"),
-    path('service/car-diagnosis', views.ACService.as_view(),name="diagnosis"),
-    path('service/body-work-and-paint', views.ACService.as_view(),name="body"),
-    path('service/original-service-parts', views.ACService.as_view(),name="original"),
+    path('service/car-diagnosis', views.CarDiagnosis.as_view(),name="diagnosis"),
+    path('service/body-work-and-paint', views.BodyWorkpaint.as_view(),name="body"),
+    path('service/original-service-parts', views.OriginalService.as_view(),name="original"),
 ]
