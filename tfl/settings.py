3581ce51694ac602775e6428d394dbee24172007
code <hi@sammagafu.com>
2021-11-02 12:51:54 +0300

    updated staticfiles

--- tfl/settings.py
+++ tfl/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 if not DEBUG:
     STATIC_ROOT = '/home/sammy/tfl/staticfiles/'
