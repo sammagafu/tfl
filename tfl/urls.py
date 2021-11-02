97ebe1632d9f75dad4128b83f9d34e417fd206f2
code <hi@sammagafu.com>
2021-11-02 13:04:23 +0300

    updated url.py to accept settings for media and static

--- tfl/urls.py
+++ tfl/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('',include('pages.urls')),
     path('sell/',include('sellyourcar.urls')),
     path('buy/',include('buyacar.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
