cc17d206ac8eee67af6bd5a731b2edd613502a3f
ubuntu temp <ubuntutemp@Stephens-MacBook-Pro.local>
2021-11-19 12:04:58 +0300

    updated work and servics

--- buyacar/urls.py
+++ buyacar/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('<str:slug>',views.GetCarsByMake.as_view(),name="make"),
     path('filter/<str:slug>',views.GetCarsByColor.as_view(),name="color"),
     # re_path(r'^(?P<slug>\D+)',views.GetCarsByMake.as_view(),name="make"),
-    path('car/<str:uuid>',views.SellingCarrsDetail.as_view(),name="detail"),
+    path('car/<str:pk>',views.SellingCarrsDetail.as_view(),name="detail"),
     path('add/',views.SellingCreateView.as_view(),name="create"),
     path('filter-by/status/used-tanzania/',views.FilterStatus.as_view(),name="tanzania")
 ]
--- static/js/main.js
+++ static/js/main.js
@@ -1080,7 +1080,7 @@
                 dots: false,
                 infinite: true,
                 speed: 300,
-                slidesToShow: 5,
+                slidesToShow: 6,
                 slidesToScroll: 1,
                 prevArrow: '<a class="slick-prev"><i class="fas fa-arrow-left" alt="Arrow Icon"></i></a>',
                 nextArrow: '<a class="slick-next"><i class="fas fa-arrow-right" alt="Arrow Icon"></i></a>',
@@ -1088,7 +1088,7 @@
                     {
                         breakpoint: 992,
                         settings: {
-                            slidesToShow: 4,
+                            slidesToShow: 5,
                             slidesToScroll: 1
                         }
                     },
